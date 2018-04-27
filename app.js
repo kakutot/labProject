da83142ad940aa4751ba8770589addc07ba3d86b
kakutot <36713667+kakutot@users.noreply.github.com>
2018-04-27 21:59:29 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,9 +4,21 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var hbs = require('express-handlebars')
-var routes = require('./routes/index');
+
 var app = express();
 var mysql = require('mysql');
+
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/weblabdb');
+var db = mongoose.connection;
+
+var routes = require('./routes/index');
+var users =  require('./routes/users');
 
 
 
@@ -25,7 +37,35 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+//Connect Flash
+
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
+    next();
+});
+
 app.use('/', routes);
+app.use('/users', users);
+
 var connection= mysql.createConnection({
     user: 'root',
     password: '21272829ab',
@@ -33,12 +73,9 @@ var connection= mysql.createConnection({
     database: 'web_db'
 });
 
-app.post("/reservations", function(req, res) {
-    var fName = req.body.userFName;
-    var lname =  req.body.userLName;
-    var res_date = req.body.res_date;
-    var yacht = req.body.yacht;
 
+
+app.post("/reservations", function(req, res) {
     connection.connect(function(err) {
         if (err) {
             console.error('error connecting: ' + err.stack);
@@ -47,48 +84,136 @@ app.post("/reservations", function(req, res) {
         console.log('connected as id ' + connection.threadId);
     });
 
-    var post = {
-        Fname: fName,
-        Lname: lname
-    };
-    var sql = "INSERT INTO myuser SET ? ";
 
-    connection.query(sql,post ,function (err, result) {
-        if (err) throw err;
-         console.log("user record inserted");
-         console.log("user id " + result.insertId);
+        //нужно проверить есть ли вообще есть ли такая яхта
 
-         //check duplicates values
-         var sql2="SELECT * FROM myorder WHERE order_date='"+res_date +"' AND yacht_id ="+ yacht;
-         console.log(sql2)
-         connection.query(sql2, function(err,result) {
-             console.log("length" + result.length)
-             if (result.length > 0) {
-                 res.render('nosuccess',{data:req.body})
-                 connection.end();
-             }
-             else{
+        var sql2 = "SELECT * FROM yacht WHERE Yacht_Name='" + req.body.yacht+"';";
+        console.log(sql2)
+        connection.query(sql2, function(err,result) {
+            if (err) throw err;
+            if(result.length>0){
+                //check duplicates values
 
-                 var val = {
-                     yacht_id: yacht,
-                     order_date:res_date,
-                     user_id: result.insertId
-                 };
-                 connection.query("INSERT INTO myorder SET ?", val,
-                     function(err, result) {
-                         if (err) throw err;
-                         console.log("second record inserted");
-                         if(result.affectedRows>0){
-                             console.log("rows aff" + result.affectedRows)
-                             res.render('success',{data:req.body})
-                         }
-                         connection.end();
-                     });
-             }
-         });
-    });
+                var sql3 = "SELECT yacht_id FROM yacht WHERE Yacht_Name='" + req.body.yacht +"';";
+                connection.query(sql3, function(err,result) {
+                    console.log("Yacht id with such name : " + result[0].yacht_id);
+                    if (err) throw err;
+                    var sql4="SELECT * FROM myorder WHERE order_date ='" + req.body.res_date+"' AND yacht_id ="+result[0].yacht_id+";";
+                    console.log(sql4);
+                    var val = {
+                        yacht_id: result[0].yacht_id,
+                        order_date:req.body.res_date,
+                        user_id: null
+                    };
+                    connection.query(sql4, function(err,result) {
+                        if (result.length > 0) {
+                            res.render('nosuccess',{data:req.body})
+                            //connection.end();
+                        }
+                        else{
+                            var post = {
+                                Fname: req.body.userFName,
+                                Lname: req.body.userLName
+                            };
+                            var sql = "INSERT INTO myuser SET ? ";
+                            connection.query(sql,post ,function (err, result) {
+                                if (err) throw err;
+
+                                console.log("user record inserted");
+                                console.log("user id " + result.insertId);
+                                val.user_id = result.insertId;
+
+                                connection.query("INSERT INTO myorder SET ?", val,
+                                    function(err, result) {
+                                        if (err) throw err;
+                                        console.log("second record inserted");
+                                        if(result.affectedRows>0){
+                                            console.log("rows aff" + result.affectedRows)
+                                            res.render('success',{data:req.body})
+                                        }
+                                      //  connection.end();
+                                    });
+                            });
+
+                        }
+                    });
+
+                });
+            }
+            else{
+                req.flash('error_msg', 'No such yacht!');
+                res.redirect('/reservations');
+            }
+        });
 
 });
+app.post("/reservations/del", function(req, res) {
+    // connection.connect(function(err) {
+    //     if (err) {
+    //         console.error('error connecting: ' + err.stack);
+    //         return;
+    //     }
+    //     console.log('connected as id ' + connection.threadId);
+        if(req.body.yachtName2=="Mako" || req.body.yachtName2=="Quiet Time" ){
+            req.flash('error_msg', 'This yacht can not be deleted!');
+            res.redirect('/yachts');
+        }
+        else{
+            var sql = "SELECT Yacht_Name FROM yacht WHERE Yacht_Name ='" + req.body.yachtName2 + "'";
+            connection.query(sql,function (err, result) {
+                if (err) {
+                    console.error('error: ' + err.stack);
+                    return;
+                }
+                if(result.length>0){
+                    var sql2 = "DELETE FROM yacht WHERE Yacht_Name ='" + req.body.yachtName2 + "'";
+                    console.log("sql:"+sql2);
+                    connection.query(sql2,function (err, result) {
+                        if (err) {
+                            console.error('error: ' + err.stack);
+                            return;
+                        }
+                        req.flash('success_msg', 'Yacht deleted!');
+                        res.redirect('/yachts');
+
+                    });
+                }
+                else{
+                    req.flash('error_msg', 'No such yacht!');
+                    res.redirect('/reservations');
+                }
+            });
+        }
+
+
+});
+app.post("/reservations/add", function(req, res) {
+    // connection.connect(function(err) {
+    //     if (err) {
+    //         console.error('error connecting: ' + err.stack);
+    //         return;
+    //     }
+    //     console.log('connected as id ' + connection.threadId);
+    // });
+    var sqlQuery = {
+        Yacht_Name: req.body.yachtName,
+        Descr : req.body.descr
+    };
+    var sql = "INSERT INTO yacht SET ? ";
+    connection.query(sql,sqlQuery ,function (err, result) {
+        if (err) {
+            console.error('error: ' + err.stack);
+            return;
+        }
+        req.flash('success_msg', 'Yacht added !');
+        res.redirect('/yachts');
+
+        });
+});
+
+
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
