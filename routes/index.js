f1eb6e11e9b0bb52d7bf3afe8110a91dbf07fc63
kakutot <36713667+kakutot@users.noreply.github.com>
2018-04-27 22:00:55 +0300

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
+
 var mysql = require('mysql');
 /* GET home page. */
+
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Tupkalenko Lab' });
 });
@@ -11,8 +14,38 @@ router.get('/blog', function(req, res, next) {
 });
 /* GET yachts page. */
 router.get('/yachts', function(req, res, next) {
-    res.render('yachts', { title: 'Tupkalenko Lab>yachts' });
+    var connection= mysql.createConnection({
+        user: 'root',
+        password: '21272829ab',
+        host: '127.0.0.1',
+        database: 'web_db'
+    });
+    connection.connect(function(err) {
+        if (err) {
+            console.error('error connecting: ' + err.stack);
+            return;
+        }
+        console.log('connected as id ' + connection.threadId);
+    });
+    sql = "SELECT Yacht_Name,Descr FROM yacht ";
+      connection.query(sql, function(err,result,fields) {
+         if (err) {
+             console.error('error: ' + err.stack);
+             return;
+         }
+
+         var resObject = [];
+       for(var i =2 ;i<result.length;i++){
+            resObject.push({Yacht_Name:result[i].Yacht_Name,Descr:result[i].Descr});
+             console.log(resObject[i]);
+       }
+        console.log(resObject.length);
+          res.render('yachts', {resObject:resObject});
+    });
 });
+
+
+
 /* GET reservations page. */
 router.get('/reservations', function(req, res, next) {
     var connection= mysql.createConnection({
